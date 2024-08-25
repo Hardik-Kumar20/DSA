10616b95cc67e3cec7eccd90d9af3b5e20969d99
Hardik Kumar <kumarhardik821@gmail.com>
2024-08-25 00:12:51 -0700

    25-8-24

--- DSA/DSA.java
+++ DSA/DSA.java
@@ -1,7 +1,9 @@
 package DSA;
-
+import java.util.*;
 public class DSA {
     public static void main(String[] args){
         System.out.println("hello world");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt());
     }
 }
