17bfe2aa4f99a86cdb4d06fccb1937a70738b95f
Hardik Kumar <kumarhardik821@gmail.com>
2024-08-25 22:42:45 -0700

    26-8-24 11:12

--- DSA/conditions.java
+++ DSA/conditions.java
@@ -3,27 +3,65 @@ import java.util.*;
 public class conditions {
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your salary ::");
-        int salary = sc.nextInt();
-        System.out.println("Your salary is " + salary);
-        if(salary > 20000){
-            salary = salary+4000;
-            System.out.println("Salary with bonus " + salary);
-        }
-        else{
-            salary = salary+1000;
-            System.out.println(salary);
-        }
+        // System.out.println("Enter your salary ::");
+        // int salary = sc.nextInt();
+        // System.out.println("Your salary is " + salary);
+        // if(salary > 20000){
+        //     salary = salary+4000;
+        //     System.out.println("Salary with bonus " + salary);
+        // }
+        // else{
+        //     salary = salary+1000;
+        //     System.out.println(salary);
+        // }
 
-                // using while loop
-                int n=1;
-                while (n != 11) {
-                    int a = 2*n;
-                    System.out.println("2 * " + n + " = " +a);
-                    n++;
+        //         // using while loop
+        //         int n=1;
+        //         while (n != 11) {
+        //             int a = 2*n;
+        //             System.out.println("2 * " + n + " = " +a);
+        //             n++;
+        //         }
+
+
+            //     // fibonacci
+            // System.out.println("Enter the number of terms of fibonacci you want");
+            // int n = sc.nextInt();
+            // int a =0;
+            // int b=1;
+            // System.out.println(a);
+            // System.out.println(b);
+            // int count =2;
+            // while(count <= n){
+            //     int temp =b;
+            //     b = a+b;
+            //     a = temp;
+            //     count++;
+            //     System.out.println(b);
+            // }
+    
+
+
+            //find the occurence of particular digit in a number
+
+            System.out.println("Enter the number");
+            long number = sc.nextInt();
+            System.out.println("which number occurence you want to check ::");
+            int n = sc.nextInt();
+            int count = 0;
+            
+            while (number > 0) {
+                long a = number % 10;
+                if(a == n){
+                    count++;
                 }
+                number =number/10;
+            }
+            System.out.println("The number " +n + " ocuurs " +count + " times");
+    
+        }
 
-    }
+
 }
 
 
