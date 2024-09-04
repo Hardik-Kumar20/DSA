e9bcc6f9bca0fb49649d6e67f6f53d60e8f13a74
Hardik Kumar <kumarhardik821@gmail.com>
2024-09-04 10:15:16 -0700

    4-9-2024 10:45 2d array

--- DSA/Array.java
+++ DSA/Array.java
@@ -1,10 +1,29 @@
 package DSA;
-
+import java.util.*;
 public class Array {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6};
-        for(int i =0 ; i < arr.length ; i++){
-            System.out.print(arr[i] + " ");
+          
+        Scanner sc = new Scanner(System.in); 
+        
+        int[][] arr = new int[3][3];
+        for(int i =0 ; i< 3 ; i++){
+            for(int j=0 ; j<3 ; j++){
+                arr[i][j] = sc.nextInt();
+            }
+
+        }
+        for(int i =0 ; i< 3 ; i++){
+            for(int j=0 ; j<3 ; j++){
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+            
         }
     }
+
+    // //2d array 
+    // static void arr(){
+        
+
+    // }
 }
